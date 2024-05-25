abeb21cb677e8d2547c288955c8117e22fc17591
Artem Burykin <nisoartem@gmail.com>
2024-05-25 20:20:25 +0400

    removed the dark theme

--- src/assets/site/common.js
+++ src/assets/site/common.js
@@ -1,10 +1,8 @@
-import {themeToggle} from '../common/scripts/theme-toggle';
 import {stickySideNav} from '../common/scripts/sticky-side-nav';
 import {mobileMenu} from '../common/scripts/mobile-menu';
 import './common.scss';
 import {gaConversions} from '../common/scripts/ga-conversions';
 
-themeToggle('theme-toggle', 'theme-toggle');
 stickySideNav();
 mobileMenu('mobile-menu', 'header__menu-popup');
 gaConversions();
