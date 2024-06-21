3e81f73f0dae644439dbca1b668cb49e28506444
Artem Burykin <nisoartem@gmail.com>
2024-06-21 21:13:56 +0400

    removed the side menu

--- src/assets/site/common.js
+++ src/assets/site/common.js
@@ -1,8 +1,6 @@
-import {stickySideNav} from '../common/scripts/sticky-side-nav';
 import {mobileMenu} from '../common/scripts/mobile-menu';
 import './common.scss';
 import {gaConversions} from '../common/scripts/ga-conversions';
 
-stickySideNav();
 mobileMenu('mobile-menu', 'header__menu-popup');
 gaConversions();
