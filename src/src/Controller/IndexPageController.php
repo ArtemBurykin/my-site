37ff7cbd9f493beaab4c43f600646f1cf533a477
Artem Burykin <nisoartem@gmail.com>
2024-07-03 22:23:53 +0400

    started redoing of the main page

--- src/src/Controller/IndexPageController.php
+++ src/src/Controller/IndexPageController.php
@@ -3,14 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/', name: 'app_index_page')]
 class IndexPageController extends AbstractController
 {
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
-        return $this->render('site/index_page/index_page.html.twig');
+        $version = $request->get('v', '1');
+
+        if ($version === '1') {
+            return $this->render('site/index_page/index_page.html.twig');
+        } else {
+            return $this->render('site/index_page/index_page_2.html.twig');
+        }
     }
 }
