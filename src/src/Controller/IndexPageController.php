89d97639c8c76ecc4a1b5be0ef7a73e35dd80966
Artem Burykin <nisoartem@gmail.com>
2024-07-05 19:32:38 +0400

    small markup fixes, removed the draft of the main page

--- src/src/Controller/IndexPageController.php
+++ src/src/Controller/IndexPageController.php
@@ -3,21 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/', name: 'app_index_page')]
 class IndexPageController extends AbstractController
 {
-    public function __invoke(Request $request): Response
+    public function __invoke(): Response
     {
-        $version = $request->get('v', '1');
-
-        if ($version === '1') {
-            return $this->render('site/index_page/index_page.html.twig');
-        } else {
-            return $this->render('site/index_page/index_page_2.html.twig');
-        }
+        return $this->render('site/index_page/index_page.html.twig');
     }
 }
