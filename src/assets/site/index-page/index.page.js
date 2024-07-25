e7685a1a621b862908764fde447c3704db2a9b72
Artem Burykin <nisoartem@gmail.com>
2024-07-25 18:00:51 +0400

    removed the clients slider

--- src/assets/site/index-page/index.page.js
+++ src/assets/site/index-page/index.page.js
@@ -2,10 +2,8 @@ import {contactForm} from '../../common/scripts/contact-form';
 import './index.page.scss';
 import {addScrollOnOfferCtaClick} from './scroll-on-offer-cta';
 import {fileInput} from '../../common/scripts/file-input';
-import {clientsSlider} from './clients-slider';
 
 fileInput('contact-form__file-input');
 contactForm('contact-form', 'contact-form');
-clientsSlider('clients');
 
 addScrollOnOfferCtaClick();
