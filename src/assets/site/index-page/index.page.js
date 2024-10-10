46859773ce3ed11a9419989ab7be34fb394548ef
Artem Burykin <nisoartem@gmail.com>
2024-10-10 14:12:58 +0200

    added the popup trigger for the main page cta

--- src/assets/site/index-page/index.page.js
+++ src/assets/site/index-page/index.page.js
@@ -1,6 +1,5 @@
 import {contactForm} from '../../common/scripts/contact-form';
 import './index.page.scss';
-import {addScrollOnOfferCtaClick} from './scroll-on-offer-cta';
 import {fileInput} from '../../common/scripts/file-input';
 import {popup} from '../../common/scripts/popup';
 import {imagePreview} from './image-preview';
@@ -8,6 +7,5 @@ import {imagePreview} from './image-preview';
 fileInput('contact-form__file-input');
 contactForm('contact-form', 'contact-form');
 
-addScrollOnOfferCtaClick();
 popup('image-popup');
 imagePreview('preview-trigger', 'image-preview');
--- src/assets/site/index-page/scroll-on-offer-cta.js
+++
@@ -1,6 +0,0 @@
-export const addScrollOnOfferCtaClick = function() {
-    document.getElementById('offer-cta').addEventListener('click', () => {
-        const contactForm = document.getElementById('contact-form');
-        contactForm.scrollIntoView({behavior: 'smooth'});
-    });
-};
